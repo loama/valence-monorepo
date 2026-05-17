e5f92e8f0d1d8a5434ae1acd48764f2e922a64c7
Eduardo Lopez <5743857+loama@users.noreply.github.com>
2026-05-17 11:35:55 -0600

    ci: encrypt capgo live updates

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -17,7 +17,9 @@ const config: CapacitorConfig = {
       defaultChannel: "production",
       directUpdate: false,
       keepUrlPathAfterReload: true,
-      periodCheckDelay: 600
+      periodCheckDelay: 600,
+      publicKey:
+        "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAyJkCSU/UcjG2ngteZpWvVvOD7lMw8Y7wc5WTle3iCyDJaiWi6Ahf\nYOPKhrEN0yg1GPzhZn2asQe1kzje7G/VKq3dcc5R+mrXYE/Dz5ygW1Be5LtNRGZ8\nLP8BS8+qZPV8YCJyxj/acVpy8BvaLqVQTcZBlLL/iNBZaqjCko/LmrQks2dKWsg/\nZ8xEFUHLN6TSKhy1nZNweaKxPQ0QaoWcuTCbdvY7gVNJ5ixunA0pgYMOvmfHrp6h\nHTEHvLZqn+04Xnuufdocl+WFv/orPID96jLDR70UeQa+X/l6oKrmVItZXf9llA5l\nuiGwT+JT89A4nAWakcTJxdnLtwbvZzsnYQIDAQAB\n-----END RSA PUBLIC KEY-----\n"
     },
     PushNotifications: {
       presentationOptions: ["badge", "sound", "banner", "list"]
