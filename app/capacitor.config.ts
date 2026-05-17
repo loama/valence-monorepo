a2d275707d589a3648e57d7c5a4f108fdf16c7d8
Eduardo Lopez <5743857+loama@users.noreply.github.com>
2026-05-17 12:59:59 -0600

    fix(app): reset stale live update bundles

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -18,6 +18,7 @@ const config: CapacitorConfig = {
       directUpdate: false,
       keepUrlPathAfterReload: true,
       periodCheckDelay: 600,
+      resetWhenUpdate: true,
       publicKey:
         "-----BEGIN RSA PUBLIC KEY-----\nMIIBCgKCAQEAyJkCSU/UcjG2ngteZpWvVvOD7lMw8Y7wc5WTle3iCyDJaiWi6Ahf\nYOPKhrEN0yg1GPzhZn2asQe1kzje7G/VKq3dcc5R+mrXYE/Dz5ygW1Be5LtNRGZ8\nLP8BS8+qZPV8YCJyxj/acVpy8BvaLqVQTcZBlLL/iNBZaqjCko/LmrQks2dKWsg/\nZ8xEFUHLN6TSKhy1nZNweaKxPQ0QaoWcuTCbdvY7gVNJ5ixunA0pgYMOvmfHrp6h\nHTEHvLZqn+04Xnuufdocl+WFv/orPID96jLDR70UeQa+X/l6oKrmVItZXf9llA5l\nuiGwT+JT89A4nAWakcTJxdnLtwbvZzsnYQIDAQAB\n-----END RSA PUBLIC KEY-----\n"
     },
